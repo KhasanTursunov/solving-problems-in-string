b8dda24c1c546dd7261a28cf00b175aea947b3fe
KhasanTursunov <khasantursunovrustamovich@gmail.com>
2024-09-16 20:35:42 +0500

    solving problems

---
+++ main.js
@@ -0,0 +1,12 @@
+// 1-masala
+
+// let str = "Khasan K1h2a3s4a5n6";
+// let number = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//      number = str.charCodeAt(i);
+//   if (number >= 48 && number <= 57) {
+//        result =  str[i] *= 2;
+//     console.log(result);
+// };
+// };
